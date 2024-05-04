b0f4b16dfd13eaac8fc25e556a16bad672377b2e
shreyasharmaweb <shreyasharma7051@gmail.com>
2024-05-04 20:17:55 +0530

    done

--- backend-tms/routes/SystemUserRoutes.js
+++ backend-tms/routes/SystemUserRoutes.js
@@ -1,10 +1,16 @@
 const express=require('express');
 const app=express.Router();
+const expressAsyncHandler=require("express-async-handler")
 const SystemUser=require('../models/SystemUser');
 const OrganisationUser = require('../models/OrganisationUser')
+const nodemailer =require("nodemailer");
+
+
+
 app.post('/signup',async (req,res)=>{
     const {email,otp}=req.body;
     const Sys=new SystemUser({email,otp});
+    
     await Sys.save();
     res.status(201).json(Sys);
 })
--- frontend/src/components/System_User.js
+++ frontend/src/components/System_User.js
@@ -1,22 +1,45 @@
+import axios from 'axios';
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
 
 export default function System_User() {
     const [otp, setOtp] = useState("");
+    const [email,setemail]=useState("");
+//"http://localhost:3001/sys/signup"
+   const handlechange= async()=>{
+        
+            let dataSend={
+                otp:otp,
+                email:email
+            }
+            console.log(dataSend);
+         try{
+            const response = await fetch("http://localhost:3001/sys/signup",{
+                method:"POST",
+                headers:{
+                    "Content-Type":"application/json"
+                },
+                body:JSON.stringify({
+                    dataSend
+                })
+            })
+              console.log("send");
+         }catch{
+             
+         }
+   }
+  
     
 
-    // const handle = () => {
-       
-    // };
 
     return (
         <div className='m'>
             <div className='otp'>
                 <h1>System User</h1>
-                <input type='email' placeholder='Enter your email' />
-                <input type='text' placeholder='Enter otp' value={otp} onChange={(e) => setOtp(e.target.value)}></input>
-                <button>OTP</button>
+                <input type='email' placeholder='Enter your email' value={email} onChange={(e)=>{setemail(e.target.value)}}/>
+                <input type='text' placeholder='Enter otp' value={otp} onChange={(e) => setOtp(e.target.value)}/>
+                <button onClick={handlechange} >OTP</button>
                 <NavLink to='/Orgs'>View Organisations</NavLink>
                 <NavLink to='/Allusers'>View Users</NavLink>
             </div>
