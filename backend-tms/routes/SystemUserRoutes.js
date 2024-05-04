7a2b6e38a70258b70ba570e2bc34cf31195af44b
shreyasharmaweb <shreyasharma7051@gmail.com>
2024-05-04 20:31:34 +0530

    systemuser

--- backend-tms/routes/SystemUserRoutes.js
+++ backend-tms/routes/SystemUserRoutes.js
@@ -5,12 +5,39 @@ const SystemUser=require('../models/SystemUser');
 const OrganisationUser = require('../models/OrganisationUser')
 const nodemailer =require("nodemailer");
 
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  secure: false, 
+  auth: {
+    user: process.env.SMTP_MAIL,
+    pass: process.env.SMTP_PASSWORD,
+  },
+});
 
+const sendEmail=expressAsyncHandler(
+  async (req,res)=>{
+    const {email,otp}=req.body;
+
+    var mailOptions={
+      from:process.env.SMTP_MAIL,
+      to:email,
+      otp:otp,
+
+    }
+  
+    transporter.sendMail(mailOptions,function(err,info){
+       if(err){
+        console.log(err);
+       }
+    })
+
+})
 
 app.post('/signup',async (req,res)=>{
     const {email,otp}=req.body;
     const Sys=new SystemUser({email,otp});
-    
+    sendEmail(email, otp);
     await Sys.save();
     res.status(201).json(Sys);
 })
